78f5c066953ad45a3061a87949d84ec1f064f60e
adilbaev-ruslan <adilbaev-ruslan@umail.uz>
2020-09-08 22:14:57 +0500

    update template

--- app/src/main/java/com/example/baslangishmatematika/MainActivity.kt
+++ app/src/main/java/com/example/baslangishmatematika/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        playGame()
+        playGame() //play game
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
